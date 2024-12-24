fe1c39fb2807cd8ee18049a00674a3ba4a689d33
asif <khanaasif1065@gmail.com>
2024-12-24 15:20:06 +0530

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Courses from "./courses/Courses.jsx";
 import Signup from "./components/Signup.jsx";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider.jsx";
-import ContactFull from "./Contact/ContactFull.jsx"
+// import ContactFull from "./Contact/ContactFull.jsx"
 function App() {
   const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
@@ -19,7 +19,7 @@ function App() {
             element={ <Courses /> }
           />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/Contact" element={<ContactFull/>}/>
+          {/* <Route path="/Contact" element={<ContactFull/>}/> */}
         </Routes>
         <Toaster />
       </div>
