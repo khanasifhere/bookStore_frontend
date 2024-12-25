ec3ac0a54a03919cbdb6b61e55a49426835872cf
asif <khanaasif1065@gmail.com>
2024-12-25 13:13:38 +0530

    router in router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Home from "./home/Home.jsx";
-import {  Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses.jsx";
 import Signup from "./components/Signup.jsx";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider.jsx";
+// import { Router } from "express";
 // import ContactFull from "./Contact/ContactFull.jsx"
 function App() {
   const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
   return (
-    <>
+    <Router>
       <div className="dark:bg-slate-900 dark:text-white">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -23,7 +24,7 @@ function App() {
         </Routes>
         <Toaster />
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+ 
     <AuthProvider>
       <div className="dark:bg-slate-900 dark:text-white">
         <App />
       </div>
     </AuthProvider>
-  </BrowserRouter>
+  
 );
